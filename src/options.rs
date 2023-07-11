b60eeae7174d4f157ab8ecf6f8f5f54907604489
scx1332 <sieciech.czajka@golem.network>
2023-07-11 20:24:54 +0200

    Stream delay

--- src/options.rs
+++ src/options.rs
@@ -1,8 +1,11 @@
 use std::time::Duration;
 
 pub enum StreamBehavior {
+    ///Do not add delay to the stream, stream will try to catch up
     Continue,
+    ///Add delay to the stream so it won't try to catch up
     Delay(f64),
+    ///Stop stream (no longer producing elements and terminates without error)
     Stop,
 }
 
@@ -16,9 +19,13 @@ where
 
     ///none for default slippage (10 times interval + 0.02 sec)
     ///f64::max_value() for no slippage at all (stream always wants to catch up after delay)
+    ///if stream is currently delayed more than this then on_stream_delayed is called
     pub allowed_slippage_sec: Option<f64>,
 
-    ///return true if you want
+    ///return StreamBehavior::Delay to add permanent delay to stream
+    ///return StreamBehavior::Stop to stop stream (terminate without error)
+    ///return StreamBehavior::Continue to continue stream (trying catching up)
+    ///First argument is current delay, second is permanent delay already in the stream
     pub on_stream_delayed: G,
 }
 

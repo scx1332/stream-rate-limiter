6bf3bf888f7759fdedf7dbb0a9f0ce10e4edfec8
scx1332 <sieciech.czajka@golem.network>
2023-07-18 21:20:05 +0200

    Better options for rate limiter

--- src/options.rs
+++ src/options.rs
@@ -47,6 +47,7 @@ impl<'a> Default for RateLimitOptions<'a> {
 }
 
 impl<'a> RateLimitOptions<'a> {
+    ///targeted interval between items
     pub fn with_interval(mut self, interval: Duration) -> Self {
         self.interval = Some(interval);
         self
@@ -57,6 +58,7 @@ impl<'a> RateLimitOptions<'a> {
         self
     }
 
+    ///minimum interval between items
     pub fn with_min_interval(mut self, min_interval: Duration) -> Self {
         self.min_interval = Some(min_interval);
         self
@@ -67,11 +69,18 @@ impl<'a> RateLimitOptions<'a> {
         self
     }
 
+    ///none for default slippage (10 times interval + 0.02 sec)
+    ///f64::max_value() for no slippage at all (stream always wants to catch up after delay)
+    ///if stream is currently delayed more than this then on_stream_delayed is called
     pub fn with_allowed_slippage_sec(mut self, allowed_slippage_sec: f64) -> Self {
         self.allowed_slippage_sec = Some(allowed_slippage_sec);
         self
     }
 
+    ///return StreamBehavior::Delay to add permanent delay to stream
+    ///return StreamBehavior::Stop to stop stream (terminate without error)
+    ///return StreamBehavior::Continue to continue stream (trying catching up)
+    ///First argument is current delay, second is permanent delay already in the stream
     pub fn on_stream_delayed(
         mut self,
         on_stream_delayed: impl FnMut(f64, f64) -> StreamBehavior + 'a,
